d0a51e5f95247560c9b3ce4885521ac4d9350283
DillerDvondy <47392393+DillerDvondy@users.noreply.github.com>
2019-05-01 15:30:24 +0300

    Данные бота
    Добавлены примеры работы с данными бота и основново гайда по использованию.

--- start.py
+++ start.py
@@ -1,8 +1,23 @@
-token = '727281713:AAFeNruJlHcYEo_Hdb3yOrvAyNtHfFhmDvE'
+# В данном файле список команд для мониторинга запросов к боту и от бота.
+# Команды использовать ТОЛЬКО в встройном интерпритаторе <<Python IDLE>>.
+# Команды записывать СТРОГО по этой последовательности. 
 
-import requests
+token = '727281713:AAFeNruJlHcYEo_Hdb3yOrvAyNtHfFhmDvE' #Записали токен Бота
 
-BASE_URL = 'https://api.telegram.org/bot727281713:AAFeNruJlHcYEo_Hdb3yOrvAyNtHfFhmDvE/'
-r_getMe = requests.get(f'{BASE_URL}getMe')
+import requests #Импортировали библиотеку для работы с запросами
 
-r_getUpdates = requests.get(f'{BASE_URL}getUpdates')
+BASE_URL = 'https://api.telegram.org/bot727281713:AAFeNruJlHcYEo_Hdb3yOrvAyNtHfFhmDvE/' #Основная ссылка(с токеном)
+
+r = requests.get(f'{BASE_URL}getMe') #Запрос на информацию про бота
+
+r = requests.get(f'{BASE_URL}getUpdates') #Запрос на получение писем к боту
+
+payload = {} #Создание словаря ответа к юзеру
+
+payload['chat_id'] = 461367829 #ID чата
+
+payload['text'] = 'Habib won' #Текст сообщения
+
+r = requests.post(f'{BASE_URL}sendMessage', data = payload) #Запрос на ответ от бота
+
+r.json() #Команда для просмотра данных ЗАПРОСА(читать коментарии)
