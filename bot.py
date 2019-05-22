8da17880516b55e9706a4793b76ad4e1be8a5a29
DillerDvondy <47392393+DillerDvondy@users.noreply.github.com>
2019-05-22 18:04:42 +0300

    Автоматизация №3
    Создание азов бота, начало автоматизации и ответы от бота.

--- bot.py
+++ bot.py
@@ -3,20 +3,23 @@ from telebot.types import Message
 import random
 
 TOKEN = '727281713:AAFeNruJlHcYEo_Hdb3yOrvAyNtHfFhmDvE'
-
 bot = telebot.TeleBot(TOKEN)
+
+updates = bot.get_updates()
 
 smile = ['😂', '😘', '❤', '😍', '😊', '😁', '☺', '😔', '😄', '😭', '😳']
 
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Hello!")
+def send_welcome(message: Message):
+    bot.reply_to(message, 'Привет %s %s!' % (message.from_user.first_name, message.from_user.last_name))
+    print(message)
 
 
 @bot.message_handler(func=lambda message: True)
 def upper(message: Message):
     bot.reply_to(message, random.choice(smile))
+    print(message)
 
 
 bot.polling()
