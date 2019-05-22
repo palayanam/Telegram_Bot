c3740416f7f9795afe59d03794dfa8e70c30c04f
DillerDvondy <47392393+DillerDvondy@users.noreply.github.com>
2019-05-22 21:37:20 +0300

    Автоматизация №4
    Создание азов бота, начало автоматизации и ответы от бота.

--- bot.py
+++ bot.py
@@ -6,20 +6,27 @@ TOKEN = '727281713:AAFeNruJlHcYEo_Hdb3yOrvAyNtHfFhmDvE'
 bot = telebot.TeleBot(TOKEN)
 
 updates = bot.get_updates()
-
 smile = ['😂', '😘', '❤', '😍', '😊', '😁', '☺', '😔', '😄', '😭', '😳']
+user_val = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+i = 0
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start', 'help'], )
 def send_welcome(message: Message):
-    bot.reply_to(message, 'Привет %s %s!' % (message.from_user.first_name, message.from_user.last_name))
-    print(message)
+    global i
+    bot.reply_to(message, 'Привет %s!' % message.from_user.username)
+    user_val[i] = message.from_user
+    print(user_val[i])
+    i += 1
 
 
 @bot.message_handler(func=lambda message: True)
 def upper(message: Message):
+    global i
     bot.reply_to(message, random.choice(smile))
-    print(message)
+    user_val[i] = message.from_user
+    print(user_val[i], i)
+    i += 1
 
 
 bot.polling()
