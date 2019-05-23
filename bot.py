30528632ff7d03a40701cfbdb784838375f94961
DillerDvondy <47392393+DillerDvondy@users.noreply.github.com>
2019-05-23 17:55:29 +0300

    Автоматизация №5
    Создание азов бота, начало автоматизации и ответы от бота.

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 from telebot.types import Message
 import random
 
@@ -7,24 +8,29 @@ bot = telebot.TeleBot(TOKEN)
 
 updates = bot.get_updates()
 smile = ['😂', '😘', '❤', '😍', '😊', '😁', '☺', '😔', '😄', '😭', '😳']
-user_val = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+user_val = [{}]
 i = 0
+
+markup = types.ReplyKeyboardMarkup()
+itembtna = types.KeyboardButton('Cords')
+itembtnv = types.KeyboardButton('Data')
+markup.row(itembtna, itembtnv)
 
 
 @bot.message_handler(commands=['start', 'help'], )
 def send_welcome(message: Message):
     global i
-    bot.reply_to(message, 'Привет %s!' % message.from_user.username)
-    user_val[i] = message.from_user
-    print(user_val[i])
+    bot.send_message(message.from_user.id, 'Look:', reply_markup=markup)
+    user_val.insert(i, message.from_user)
+    print(user_val[i], i)
     i += 1
 
 
 @bot.message_handler(func=lambda message: True)
 def upper(message: Message):
     global i
-    bot.reply_to(message, random.choice(smile))
-    user_val[i] = message.from_user
+    bot.send_location(message.from_user.id, -19.79831, 32.70595, reply_markup=markup)
+    user_val.insert(i, message.from_user)
     print(user_val[i], i)
     i += 1
 
